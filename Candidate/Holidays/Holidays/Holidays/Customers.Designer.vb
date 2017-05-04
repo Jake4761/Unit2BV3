a85c5b8b4a01b55b768ddeba924808dce0f8a155
Jake4761 <jakemifsud79@gmail.com>
2017-05-04 13:51:41 +0100

    Add files via upload

---
+++ Candidate/Holidays/Holidays/Holidays/Customers.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtSessionID = New System.Windows.Forms.TextBox()
+        Me.lblPhoneNumber = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.cmbSearch = New System.Windows.Forms.ComboBox()
+        Me.btnSwapFormHolidays = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnSave.Location = New System.Drawing.Point(95, 251)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(96, 32)
+        Me.btnSave.TabIndex = 31
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(90, 65)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(129, 29)
+        Me.lblName.TabIndex = 25
+        Me.lblName.Text = "Customers"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtEmail.Location = New System.Drawing.Point(182, 207)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(112, 20)
+        Me.txtEmail.TabIndex = 24
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(182, 183)
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(112, 20)
+        Me.txtPhoneNumber.TabIndex = 23
+        '
+        'txtLastName
+        '
+        Me.txtLastName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtLastName.Location = New System.Drawing.Point(182, 159)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(112, 20)
+        Me.txtLastName.TabIndex = 22
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.AcceptsReturn = True
+        Me.txtFirstName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtFirstName.Location = New System.Drawing.Point(182, 135)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(112, 20)
+        Me.txtFirstName.TabIndex = 21
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(92, 207)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(73, 13)
+        Me.lblEmail.TabIndex = 30
+        Me.lblEmail.Text = "Email Address"
+        '
+        'txtSessionID
+        '
+        Me.txtSessionID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtSessionID.Location = New System.Drawing.Point(182, 111)
+        Me.txtSessionID.Name = "txtSessionID"
+        Me.txtSessionID.ReadOnly = True
+        Me.txtSessionID.Size = New System.Drawing.Size(112, 20)
+        Me.txtSessionID.TabIndex = 20
+        '
+        'lblPhoneNumber
+        '
+        Me.lblPhoneNumber.AutoSize = True
+        Me.lblPhoneNumber.Location = New System.Drawing.Point(92, 183)
+        Me.lblPhoneNumber.Name = "lblPhoneNumber"
+        Me.lblPhoneNumber.Size = New System.Drawing.Size(78, 13)
+        Me.lblPhoneNumber.TabIndex = 29
+        Me.lblPhoneNumber.Text = "Phone Number"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(92, 159)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(55, 13)
+        Me.lblLastName.TabIndex = 28
+        Me.lblLastName.Text = "LastName"
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(92, 135)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFirstName.TabIndex = 27
+        Me.lblFirstName.Text = "First Name"
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(92, 111)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(58, 13)
+        Me.lblID.TabIndex = 26
+        Me.lblID.Text = "Session ID"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnSearch.Location = New System.Drawing.Point(95, 289)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(96, 32)
+        Me.btnSearch.TabIndex = 19
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = False
+        '
+        'cmbSearch
+        '
+        Me.cmbSearch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbSearch.FormattingEnabled = True
+        Me.cmbSearch.Location = New System.Drawing.Point(197, 296)
+        Me.cmbSearch.Name = "cmbSearch"
+        Me.cmbSearch.Size = New System.Drawing.Size(121, 21)
+        Me.cmbSearch.TabIndex = 32
+        '
+        'btnSwapFormHolidays
+        '
+        Me.btnSwapFormHolidays.Location = New System.Drawing.Point(98, 326)
+        Me.btnSwapFormHolidays.Name = "btnSwapFormHolidays"
+        Me.btnSwapFormHolidays.Size = New System.Drawing.Size(93, 60)
+        Me.btnSwapFormHolidays.TabIndex = 33
+        Me.btnSwapFormHolidays.Text = "Swap to Customers Form"
+        Me.btnSwapFormHolidays.UseVisualStyleBackColor = True
+        '
+        'Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(372, 419)
+        Me.Controls.Add(Me.btnSwapFormHolidays)
+        Me.Controls.Add(Me.cmbSearch)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtPhoneNumber)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.txtSessionID)
+        Me.Controls.Add(Me.lblPhoneNumber)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Name = "Customers"
+        Me.Text = "Customers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhoneNumber As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents txtSessionID As System.Windows.Forms.TextBox
+    Friend WithEvents lblPhoneNumber As System.Windows.Forms.Label
+    Private WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents lblFirstName As System.Windows.Forms.Label
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents cmbSearch As System.Windows.Forms.ComboBox
+    Friend WithEvents btnSwapFormHolidays As System.Windows.Forms.Button
+End Class
---
+++ Candidate/Holidays/Holidays/Holidays/Customers.vb
@@ -0,0 +1,149 @@
+﻿Imports System.IO
+Imports System.Net.Mail
+Public Class Customers
+    Dim Last As Integer
+    Private Structure Customer
+        Public SessionID As String
+        Public FirstName As String
+        Public LastName As String                'Creating the structure that will hold the data.
+        Public Email As String
+        Public PhoneNumber As String
+    End Structure
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+
+        'Declaring varibales and assigning values if appropriate.
+        Dim SessionID As String
+        Dim Firstname As String = txtFirstName.Text
+        Dim LastName As String = txtLastName.Text
+        Dim Email As String = txtEmail.Text
+        Dim PhoneNumber As String = txtPhoneNumber.Text
+        Dim Validated As Boolean = True
+
+
+        'this is a simple length check, Using the .length method to check the length of the phone number 
+        'and then comparing it to 11, if it is not 11 then the phone number is invalid and an error message displayed
+        ' The validation variable will become false
+        If PhoneNumber.Length <> 11 Then
+            Validated = False
+            MsgBox("Please ensure that the phone number is 11 numbers long")
+        End If
+
+
+        'Here i am using the IsNumeric Function which returns a boolean value of whether or not the data passed is numeric.
+        'i am checking if this is false, meaning it is not numeric, if this is so an error message is displayed. The validation variable will become false
+        If IsNumeric(PhoneNumber) = False Then
+            Validated = False
+            MsgBox("Please ensure the phone number only encludes numbers and not letters")
+        End If
+
+        'these are four simple presence checks, if the variable has no data within them, meaning they are empty, then 
+        'the user is told to put data in them, The validation variable will become false
+        If Firstname = "" Or LastName = "" Or Email = "" Or PhoneNumber = "" Then
+            Validated = False
+            MsgBox("Please make sure every field is full and not empty")
+        End If
+
+        'Here i am utilising the try catch method, it will try to pass the value of email into a variable which is defined as an email
+        'if this doesnt work it is due to the Email not being of the correct format, this is called a format check.
+        'If it fails then when it is caught then an error message is displayed. The validation variable will become false
+        Try
+            Dim Test As New MailAddress(Email)
+        Catch
+
+            Validated = False
+            MsgBox("Please make sure the email is correct and in the right format")
+
+        End Try
+
+        'If none of the above validations changed "Validated" into False then this will be run, if they have then it will be skipped.
+        If Validated = True Then
+
+
+            'Here i am checking if there are any items in the combobox then a value of "1000" will be given to sessionID
+            'This is the value the first entry will take.
+            If cmbSearch.Items.Count = 0 Then
+                SessionID = 1000
+
+            Else
+                'Else if there are values in the combobox then i wil get the last value from the combo box and add 1 to it
+                'this is essentially incrementing the id
+                SessionID = Val((cmbSearch.GetItemText(cmbSearch.Items(cmbSearch.Items.Count - 1)))) + 1
+            End If
+
+            'Here i am adding the new ID to the combobox
+            cmbSearch.Items.Add(SessionID)
+
+
+            'Here i am going to be writing the data into a text file, created earlier
+            Dim CustomerData As New Customer
+            'This code is assigning the values to the structure above.
+            Dim sw As New System.IO.StreamWriter("Customer.txt", True)
+            CustomerData.SessionID = LSet(SessionID, 50)
+            CustomerData.FirstName = LSet(Firstname, 50)
+            CustomerData.LastName = LSet(LastName, 50)
+            CustomerData.Email = LSet(Email, 50)                      'Filling the structure with data.
+            CustomerData.PhoneNumber = LSet(PhoneNumber, 50)
+            'this is actually writing the values from the structure into the text file in a certain order.
+            sw.WriteLine(CustomerData.SessionID & CustomerData.FirstName & CustomerData.LastName & CustomerData.Email & CustomerData.PhoneNumber)
+            sw.Close()                                                                  'Always need to close afterwards
+            MsgBox("File Saved!")
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnSearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+        'Here i am reading the text file
+        Dim CustomerRead() As String = File.ReadAllLines("Customer.txt")
+
+        'This loop will loop through each line of the text file
+        For i = 0 To UBound(CustomerRead)
+            'If the ID in the text file matches the searchID then the values from the text file will be put into the textboxes 
+            If Trim(Mid(CustomerRead(i), 1, 50)) = cmbSearch.Text Then
+                txtSessionID.Text = Trim(Mid(CustomerRead(i), 1, 50))
+                txtFirstName.Text = Trim(Mid(CustomerRead(i), 51, 50))
+                txtLastName.Text = Trim(Mid(CustomerRead(i), 101, 50))
+                txtEmail.Text = Trim(Mid(CustomerRead(i), 151, 50))
+                txtPhoneNumber.Text = Trim(Mid(CustomerRead(i), 201, 50))
+
+            End If
+
+        Next i
+    End Sub
+
+    Private Sub btnSwapFormHolidays_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSwapFormHolidays.Click
+
+        'this button here will hide this form and show the other form
+
+        Dim Form As Form
+        Form = New Holidays
+        Form.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Customers_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+        If Dir$("Customer.txt") = "" Then
+            Dim sw As New StreamWriter("Customer.txt", True)    'This makes sure there is actually a database to enter/read data. If not, it creates a new blank one.
+            sw.WriteLine("")
+            sw.Close()
+            MsgBox("A new file has been created", vbExclamation, "Warning!")
+        End If
+
+
+        'This reads the text file
+        Dim ComboAdd() As String = File.ReadAllLines("customer.txt")
+
+        'This will read line by line the text file and add each value of the ID into the combobox
+        For i = 0 To UBound(ComboAdd)
+
+            cmbSearch.Items.Add(Trim(Mid(ComboAdd(i), 1, 50)))
+
+        Next i
+
+
+
+    End Sub
+End Class
